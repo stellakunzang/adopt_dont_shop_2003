e2d28ffc7aaa58d32b1a3debc43fbb7afeef9d14
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-04 21:50:28 -0600

    Add create method

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,21 +1,21 @@
 class SheltersController < ApplicationController
   def index
-    @shelters = ['Shelter 1', 'Shelter 2']
+    @shelters = Shelter.all 
   end
 
   def new
   end
 
   def create
-    # shelter = Shelter.new({
-    #   name: params[:shelter][:name],
-    #   address: params[:shelter][:address],
-    #   city: params[:shelter][:city],
-    #   state: params[:shelter][:state],
-    #   zip: params[:shelter][:zip]
-    #   })
-    # shelter.save
-    # redirect_to '/shelters'
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+    shelter.save
+    redirect_to '/shelters'
   end
   #
   # def show
