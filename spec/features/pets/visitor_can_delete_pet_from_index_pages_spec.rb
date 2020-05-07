75ad54cee8323a7715ec30c9665fe9c2c04332be
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-06 22:38:25 -0600

    Add delete link to pet and shelter pet index

--- spec/features/pets/visitor_can_delete_pet_from_index_pages_spec.rb
+++ spec/features/pets/visitor_can_delete_pet_from_index_pages_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "pet update", type: :feature do
-  it "can update pet from index page" do
+RSpec.describe "pet delete", type: :feature do
+  it "can delete pet from index page" do
     shelter_1 = Shelter.create(name: "Happy Puppies", address: "55 Street st", city: "Newark", state: "NK", zip: "80304")
     pet_1 = Pet.create(image: "dog.jpg", name: "Donald Duck", description: "Certified cutie patootie", approximate_age: "8", sex: "male", shelter_id: shelter_1.id)
     visit "/pets"
-    click_link "Edit Pet Information"
-    expect(current_path).to eq("/pets/#{pet_1.id}/edit")
+    click_link "Delete Pet"
+    expect(current_path).to eq("/pets")
+    expect(page).to have_no_content("Donald Duck")
   end
 
   it "can update pet from shelter pets page" do
     shelter_1 = Shelter.create(name: "Happy Puppies", address: "55 Street st", city: "Newark", state: "NK", zip: "80304")
     pet_1 = Pet.create(image: "dog.jpg", name: "Donald Duck", description: "Certified cutie patootie", approximate_age: "8", sex: "male", shelter_id: shelter_1.id)
     visit "/shelters/#{shelter_1.id}/pets"
-    click_link "Edit Pet Information"
-    expect(current_path).to eq("/pets/#{pet_1.id}/edit")
+    click_link "Delete Pet"
+    expect(current_path).to eq("/pets")
+    expect(page).to have_no_content("Donald Duck")
   end
 end
