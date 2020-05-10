25ab76324a9cadf30fc1f089243cddc25d838ef0
stellakunzang <stella.k.bonnie@gmail.com>
2020-05-10 12:40:23 -0600

    Add validations to models and specs

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,7 +1,14 @@
 require "rails_helper"
 
-RSpec.describe Shelter do
+RSpec.describe Pet do
   describe "relationships" do
-    it {should have_many :pets}
+    it { should belong_to :shelter}
   end
+  describe "validations" do
+    it { should validate_presence_of :image }
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :approximate_age}
+    it { should validate_presence_of :sex}
+  end
+
 end
